bcf471f6942df1fcb1745fcd7fbffc88f43a9bc6
Megan Bailey <megan-bailey@outlook.com>
2020-04-01 11:40:55 -0700

    remove darksky key

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.json());
 app.use(express.static('public'));
 
 app.post('/weather', (req, res) => {
-  const url = `https://api.darksky.net/forecast/c3e551f48e3c56baf9fb50a8ae8d2444/${req.body.latitude},${req.body.longitude}?units=auto`;
+  const url = `https://api.darksky.net/forecast/${YOUR_API_KEY}/${req.body.latitude},${req.body.longitude}?units=auto`;
   axios({
     url: url,
     responseType: 'json'
